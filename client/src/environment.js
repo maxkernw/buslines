dd0b6ba7e757141956581779038b0ce4cabb6711
Maximilian Kern <maxkernw@gmail.com>
2023-01-26 14:27:41 +0100

    Update environment.js

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,5 +1,3 @@
 export default {
-    ENDPOINT: 'http://localhost:8080/buslines?limit=10',
-    MAPBOX_KEY: 'pk.eyJ1IjoibWF4a2VybnciLCJhIjoiY2tlMDl6YWt3MXc4ajJxcGk0NW8zcG5xcCJ9.7aLQ5aR1kUujRSGISohWtA',
-    MAPOX_STYLE_URL:'mapbox://styles/maxkernw/ckea2vfg620t31amx93qx7va2'
+
 }
